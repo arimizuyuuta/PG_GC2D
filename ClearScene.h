8496787917d18278735273f4d4a1e0d19949a246
arimizuyuuta <k024g1028@g.neec.ac.jp>
2025-08-12 10:51:17 +0900

    AAA

--- ClearScene.h
+++ ClearScene.h
@@ -1,22 +1,12 @@
 #pragma once
+
 #include "IScene.h"
-#include <memory>
-#include <vector>
 
-class Player;
-class Bullet;
-class Enemy;
+// クリアシーン
+class ClearScene : public IScene {
 
-class StageScene : public IScene {
 public:
 	void Initialize() override;
 	void Update(char* keys, char* preKeys) override;
 	void Draw() override;
-
-private:
-	std::unique_ptr<Player> player_;
-	std::unique_ptr<Enemy> enemy_;
-	std::vector<Bullet> bullets_;
-
-	static bool AABB(float l1, float t1, float r1, float b1, float l2, float t2, float r2, float b2) { return !(r1 < l2 || r2 < l1 || b1 < t2 || b2 < t1); }
 };
